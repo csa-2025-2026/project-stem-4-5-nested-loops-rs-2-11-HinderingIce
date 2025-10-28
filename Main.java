6c71aa36ca5a245bfb95e269c8313572ea18ae10
HinderingIce <145399087+HinderingIce@users.noreply.github.com>
2025-10-28 18:22:48 +0000

    Commit

--- Main.java
+++ Main.java
@@ -4,31 +4,71 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for(int i = 0; i < word.length(); i++)
+    {
+      String current = word.substring(i, i + 1);
+      for(int e = 1; e <= N; e++)
+      {
+         System.out.print(current);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for(int i = 10; i >= 1; i--)
+    {
+      for(int j = 1; j <= i; j++)
+      {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for(int i = 1; i <= N; i++)
+    {
+      for(int e = 1; e <= i; e++)
+      {
+        System.out.print(e + " ");
+      }
+      System.out.println();
+    }
   }
 
-  public static void starTree()
+  public static void starTree(int N)
   {
-    // code solution here
+    for(int i = 1; i <= N; i++)
+    {
+      for(int a = 1; a <= i; a++)
+        {
+          System.out.print(" ");
+        }
+
+      for(int e = N - i; e >= 0; e--)
+      {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
   }
 
   public static void multTable()
   {
-    // code solution here
+    for(int i = 1;i <= 10; i++)
+    {
+      for(int e = 1;e <= 10; e++)
+      {
+          System.out.print(i * e + " ");
+      }
+      System.out.println();
+    }
   }
 }
